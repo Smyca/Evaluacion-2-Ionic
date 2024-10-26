ce02f04bdfdcb01be5d6b2e30bc3ec980869023d
POAPCA02LC0107 <SSDD@POAPCA02LC0107.duoc.acad>
2024-10-26 09:49:33 -0300

    Consumo de servicio API en Modulo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  items:any[]=[]
+  constructor(private apiService: APIService) {}
 
-  constructor() {}
+  ngOnInit() {
+
+    this.apiService.obtenerInfo().subscribe((Root)=>{
+      console.log(Root)
+      this.items=Root
+
+    localStorage.setItem('API', JSON.stringify(this.items))
+  
+  });
+    
+  }
+
+
+
 
 }
