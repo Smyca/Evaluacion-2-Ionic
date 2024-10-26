0a48f8cf4048a5dbf308e587c2cbdda8b6add600
POAPCA02LC0107 <SSDD@POAPCA02LC0107.duoc.acad>
2024-10-26 08:41:11 -0300

    Creacion de servicio API

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SamuelAlarcon203982402',
+  webDir: 'www'
+};
+
+export default config;
